e6f6714c6f185e3da9722dd03d22547cb0a3f694
Dan Revel <drevel@opensky.com>
2013-10-18 11:16:58 -0700

    ignore .idea/

--- send.php
+++ send.php
@@ -20,7 +20,7 @@ $rabbit = new Rabbit($host, $port, $user, $pass, $exchange, $vhost);
 
 $msg_body = "Fanmail from a flounder.";
 
-$properties = array('content_type'  => 'text/plain', 
+$properties = array('content_type'  => 'text/plain',
                     'delivery_mode' => 1); // 1 == not persistent, 2 == persistent
 
 for ($i = 1; $i <= $count; $i++) {
